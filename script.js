b465411447f80326144dcbf795e2479ed647eabc
Aditya <kumaradityap123@gmail.com>
2026-06-14 14:05:43 +0530

    automatic typing function

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typing", {
+    strings: [
+        "Frontend Developer",
+        "React Developer",
+        "AWS Learner",
+        "BTech Student"
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+});
